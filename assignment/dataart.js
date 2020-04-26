ab66bef148741ccd4c25ebeca452786e252d3e05
Knorton2 <60739954+Knorton2@users.noreply.github.com>
2020-04-26 02:21:02 +0100

    Art working - just needs api
    Api needs to manipulate numbers in the art. Other than that, data art is done

--- assignment/dataart.js
+++ assignment/dataart.js
@@ -2,7 +2,7 @@
 function dataart(){
 
 var n, particles;
-var circleFill = "#ff0000";
+var circleFill = "#ff0000"; //deaths
 var circRadius = 5;
 var mouseRadius = 5;
 
@@ -32,7 +32,7 @@ context = canvas.getContext('2d');
 
 // Create particles
 particles = [];
-n = 2;
+n = 4;
 for(var i = 0; i < n; i++)
 {
   var sx = Math.random() * canvas.width;
@@ -88,103 +88,13 @@ var mouseMove = function (e) {
 };
 } //Closes function
 
-//------------------------------------------------------------------------------------------
-
-
-function dataart2(){
-var n, particles;
-var circleFill = "#48a832";
-var circRadius = 5;
-var mouseRadius = 5;
-
-console.log(covid.NewConfirmed);
-
-var tid = setInterval( function () {
-  if ( document.readyState !== 'complete' ) return;
-  clearInterval( tid );
-  init();
-}, 100 );
-
-var Particle = function (sx, sy) {
-  this.sx = sx;
-  this.sy = sy;
-  this.vx = 0.0;
-  this.vy = 0.0;
-
-  this.vx = Math.random() * 2 - 1;
-  this.vy = Math.random() * 2 - 1;
-};
-
-function init()
-{
-/* Resize Canvas */
-canvas = document.getElementById("myCanvas2");
-context = canvas.getContext('2d');
-
-// Create particles
-particles = [];
-n = 7;
-for(var i = 0; i < n; i++)
-{
-  var sx = Math.random() * canvas.width;
-  var sy = Math.random() * canvas.height;
-  particles[i] = new Particle(sx, sy);
-}
-
-// Render Update
-canvas.addEventListener("mousemove", mouseMove2);
-update();
-}
-
-function update()
-{
-
-drawParticle();
-window.requestAnimationFrame(update);
-}
-
-function drawParticle()
-{
-context.clearRect(0, 0, canvas.width, canvas.height);
-for(var i = 0; i < n; i++)
-{
-  var circ = particles[i];
-  circ.sx += circ.vx;
-  circ.sy += circ.vy;
-
-  drawCircle(circ.sx, circ.sy, circRadius);
-
-  // Bounce
-  if(circ.sx < 0) circ.vx = -circ.vx;
-  if(circ.sy < 0) circ.vy = -circ.vy;
-  if(circ.sx > canvas.width) circ.vx = -circ.vx;
-  if(circ.sy > canvas.height) circ.vy = -circ.vy;
-
-  // Mouse Push
-}
-}
-
-function drawCircle(x, y, r) {
-  context.fillStyle = circleFill;
-  context.beginPath();
-  context.arc(x, y, r, 0.0, Math.PI * 2.0);
-  context.closePath();
-  context.fill();
-}
-
-var mouseMove = function (e) {
-  mouseX = e.offsetX;
-  mouseY = e.offsetY;
-};
-} //Closes function
-
 
 //-------------------------------------------------------------------------------------
 
 function dataart2(){
 var n, particles;
-var circleFill = "#48a832";
-var circRadius = 5;
+var circleFill = "#48a832"; // Recovered
+var circRadius = 20;
 var mouseRadius = 5;
 
 console.log(covid.NewConfirmed);
@@ -213,7 +123,7 @@ context2 = canvas.getContext('2d');
 
 // Create particles
 particles = [];
-n = 7;
+n = 10;
 for(var i = 0; i < n; i++)
 {
   var sx = Math.random() * canvas.width;
@@ -272,8 +182,8 @@ var mouseMove = function (e) {
 
 function dataart3(){
 var n, particles;
-var circleFill = "#ffd000";
-var circRadius = 5;
+var circleFill = "#ffd000"; //Infected
+var circRadius = 10;
 var mouseRadius = 5;
 
 console.log(covid.NewConfirmed);
@@ -302,7 +212,7 @@ context3 = canvas.getContext('2d');
 
 // Create particles
 particles = [];
-n = 7;
+n = 6;
 for(var i = 0; i < n; i++)
 {
   var sx = Math.random() * canvas.width;
